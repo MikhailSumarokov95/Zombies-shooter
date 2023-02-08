d65c54dbce7ff9f9cf38039fb8b82aecded5858e
MikhailSumarokov95 <108895364+MikhailSumarokov95@users.noreply.github.com>
2023-02-08 23:57:57 +0500

    0.60
    add LOD Group

---
+++ Zombies-shooter/Assets/Tools/SearchMehRenderer.cs
@@ -0,0 +1,33 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SearchMeshRenderer : MonoBehaviour
+{
+    [SerializeField] private LODGroup lodPref;
+
+    [SerializeField] private MeshRenderer meshRenderer;
+    [ContextMenu("MeshSearch")]
+    public void MeshSearch()
+    {
+        var meshs = FindObjectsOfType<MeshRenderer>();
+
+        for (var i = 0; i < meshs.Length; i ++)
+        {
+            if (meshs[i].transform.parent?.GetComponent<MeshRenderer>() == null)
+            {
+                var meshLod = meshs[i].AddComponent<LODGroup>();
+                meshLod.SetLODs(lodPref.GetLODs());
+                
+                var meshRed = meshs[i].GetComponent<MeshRenderer>();
+
+                var meshLods = meshLod.GetLODs();
+
+                meshLods[0].renderers[0] = meshRed;
+
+                meshLod.size = 1;
+
+                meshLod.SetLODs(meshLods);
+            }
+        }
+    }
+}
