1cc9ca60e07aef19af36760c2b8124cb30fc0a3c
MikhailSumarokov95 <108895364+MikhailSumarokov95@users.noreply.github.com>
2023-02-11 15:31:04 +0500

    0.75

--- Zombies-shooter/Assets/Scripts/TouchSystem.cs
+++ Zombies-shooter/Assets/Scripts/TouchSystem.cs
@@ -5,14 +5,14 @@ public class TouchSystem : MonoBehaviour, IDragHandler
 { 
     public static Vector3 Move = Vector2.zero;
 
-    private void OnEnable()
-    {
-        if (!FindObjectOfType<LevelManager>().IsMobile)
-        {
-            gameObject.SetActive(false);
-            Destroy(this);
-        }
-    }
+    //private void OnEnable()
+    //{
+    //    if (!FindObjectOfType<LevelManager>().IsMobile)
+    //    {
+    //        gameObject.SetActive(false);
+    //        Destroy(this);
+    //    }
+    //}
 
     private void LateUpdate() => Move = Vector3.zero;
 
