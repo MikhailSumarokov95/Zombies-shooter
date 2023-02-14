9edf17cb5aec5f16231010ff4d86ff17ddd14bee
MikhailSumarokov95 <108895364+MikhailSumarokov95@users.noreply.github.com>
2023-02-14 15:59:10 +0500

    0.82
    Add guadePC

--- Zombies-shooter/Assets/UI/Scripts/MobileUIController.cs
+++ Zombies-shooter/Assets/UI/Scripts/MobileUIController.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class MobileUIController : MonoBehaviour
 {
-    [SerializeField] private GameObject[] mobileUI;
+    [SerializeField] private GameObject[] mobileElementsUI;
+    [SerializeField] private GameObject[] pcElementsUI;
 
     private void Start()
     {
         var isMobile = FindObjectOfType<LevelManager>().IsMobile;
 
-        foreach(var UI in mobileUI)
+        foreach(var UI in mobileElementsUI)
         {
             UI.SetActive(isMobile);
+        }
+        
+        foreach(var UI in pcElementsUI)
+        {
+            UI.SetActive(!isMobile);
         }
     }
 }
